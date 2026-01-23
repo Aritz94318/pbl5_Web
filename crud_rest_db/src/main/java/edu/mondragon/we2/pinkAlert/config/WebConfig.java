6b954e799a62d61971e985993a9a945cd1e4541a
aroacalvo <aroa.calvo@alumni.mondragon.edu>
2026-01-23 09:46:25 +0100

    Demo 8try

---
+++ crud_rest_db/src/main/java/edu/mondragon/we2/pinkAlert/config/WebConfig.java
@@ -0,0 +1,14 @@
+package edu.mondragon.we2.pinkAlert.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/previews/**")
+                .addResourceLocations("file:/tmp/previews/");
+    }
+}
